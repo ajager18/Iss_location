412156c883c07f11ab85f9eac4bf4a1f59f4d2f8
ajager18 <55111352+ajager18@users.noreply.github.com>
2020-09-16 14:45:54 +0200

    Add files via upload
    Initial upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let i =1;
+let lat=0;
+let lon=0;
+
+
+	fetch('http://api.open-notify.org/iss-now.json').then(res=>res.json())
+	.then(geo=>{lat=geo.iss_position.latitude;lon=geo.iss_position.longitude;})
+	var mymap = L.map('mapid').setView([lat, lon], 5);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2VybmVsMzEiLCJhIjoiY2tmMnpqd2VtMTdjazJxcGhhYjlmczR4bSJ9.KKtK-LZdugqOApL8C9dFQg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+var marker = L.marker([lat,lon]).addTo(mymap);
+var circle = L.circle ([lat,lon],{
+	color: 'green',
+	fillColor: '#f02',
+	radius: 1500
+}).addTo(mymap);
+
+
+
+const move = () => {
+	fetch('http://api.open-notify.org/iss-now.json').then(res=>res.json())
+	.then(geo=>{lat=geo.iss_position.latitude;lon=geo.iss_position.longitude;})
+	setTimeout(function () {mymap.panTo([lat,lon]);marker.setLatLng([lat,lon]);circle.setLatLng([lat,lon]);i++;if (i>0){
+		move()
+	}},5000)
+}
+
+move();
